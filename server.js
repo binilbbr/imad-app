76b0e636054016f21ff9500009802b46ec3e4f69
binilbbr <binilbiju.mthss@gmail.com>
2017-08-22 16:27:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-1',function(req,res){
-    res.send("article one requested");
+app.get('/article-one',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-2',function(req,res){
