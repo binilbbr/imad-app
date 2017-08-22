08894ee8e2e5e38a8325d66afc5c7c91abdab893
binilbbr <binilbiju.mthss@gmail.com>
2017-08-22 20:36:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
 //move
-var madi=document.getElementById('madi');
+var mad=document.getElementById('madi');
 var marginleft=0;
 function moveRight(){
     marginleft=marginleft+10;
-    madi.style.marginleft=marginleft+'px';  
+    mad.style.marginleft=marginleft + 'px';  
 }
 
-madi.onclick=function(){
+mad.onclick =function() {
     var interval=setInterval(moveRight,50);
 }
