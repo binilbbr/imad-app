825fbc5974e4f14fddc149f7f87bd388f91c1626
binilbbr <binilbiju.mthss@gmail.com>
2017-08-22 20:52:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ function moveRight(){
     mad.style.marginleft = marginleft + 'px';  
 }
 mad.onclick = function() {
-    var interval=setInterval(moveRight,100);
+    setInterval(moveRight(),100);
 };
