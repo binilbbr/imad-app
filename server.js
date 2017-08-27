249f99752c1dcc80027a96c93e448af62ed04838
binilbbr <binilbiju.mthss@gmail.com>
2017-08-27 12:18:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,12 @@ app.get('/counter',function(req,res){
     counter =counter +1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit/:name',function(req,res){
+    var name=req.params.name;
+    names=push(name);
+    res.send(JSON.stringify(names));
+});
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
