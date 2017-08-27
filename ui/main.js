52756a457f3daa78c30bae3e51e4f6e3251179ea
binilbbr <binilbiju.mthss@gmail.com>
2017-08-27 10:47:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-var bmonitor=getElementById('fbut');
+var bmonitor=document.getElementById('fbut');
 var counter = 0;
 bmonitor.onclick= function()
 {
     counter =counter+1;
-    var spann= getElementById('noc');
+    var spann= document.getElementById('noc');
     spann.innerHTML = counter.toString();
 }
