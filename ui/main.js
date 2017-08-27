36d9187bfc267eca01cf251ff5457b1456daaa53
binilbbr <binilbiju.mthss@gmail.com>
2017-08-27 11:38:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ bmonitor.onclick= function()
     var request= new XMLHttpRequest()
     //waiting for request and store response in a variable
     request.onreadystatechange= function(){
-        if(request.readystate === DONE){
-            if(request.status ===200){
+        if(request.readystate == XMLHttpRequest.DONE){
+            if(request.status == 200){
                 var counter =request.responseText;
                 var spann= document.getElementById('noc');
                 spann.innerHTML = counter.toString();
