c6b1186246a02899f1d16c5464c54fa939965825
binilbbr <binilbiju.mthss@gmail.com>
2017-08-27 12:08:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,6 @@ submit.onclick= function(){
     for(var i=0;i<names.length;i++){
         list+='<li>' + names[i] + '</li>';
     }
-    var namelist=document.getElementBYId('list');
+    var namelist=document.getElementById('list');
     namelist.innerHTML=list;
 };
